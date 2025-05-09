c9a11876e45884310b35f4fc37bb47c0103cbcad
Rohan7585 <rohan178.rd@gmail.com>
2025-05-09 08:18:20 +0530

    Logger added

--- ConnectDB/src/main/java/com/example/ConnectDB/controller/ConnectDBController.java
+++ ConnectDB/src/main/java/com/example/ConnectDB/controller/ConnectDBController.java
@@ -1,6 +1,9 @@
 package com.example.ConnectDB.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ConnectDB.service.ConnectDBService;
@@ -9,16 +12,22 @@ import com.example.ConnectDB.service.ConnectDBService;
 public class ConnectDBController {
 	
 	private final ConnectDBService connectDBService;
+	private static final Logger logger = LoggerFactory.getLogger(ConnectDBController.class);
+
 	
 	public ConnectDBController(ConnectDBService connectDBService) {
 		this.connectDBService = connectDBService;
 	}
 	@GetMapping("/")
     public String home() {
+		logger.info("This is an info log");
+        logger.debug("This is a debug log");
+        logger.error("This is an error log");
         return "Welcome to Spring Boot!";
     }
 	@GetMapping("/greet")
 	public String greet() {
+		logger.info("Greet endpoint was called");
 		return connectDBService.greeting();
 	}
 }
