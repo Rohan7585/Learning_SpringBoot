6acfb0154d263be26cb808aa93bab9fa48966023
Rohan7585 <rohan178.rd@gmail.com>
2025-05-12 07:54:52 +0530

    Update and Delete added

--- Registration/src/main/java/com/sample/Registration/controller/UserController.java
+++ Registration/src/main/java/com/sample/Registration/controller/UserController.java
@@ -3,8 +3,11 @@ package com.sample.Registration.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +29,25 @@ public class UserController {
     public List<UserModel> getAllUsers() {
         return userRepository.findAll();
     }
+	
+	@PutMapping("/{id}")		//Endpoint will have the id of the user
+	public UserModel updateUser(@PathVariable Long id, @RequestBody UserModel updatedUser) {
+	    return userRepository.findById(id).map(user -> {
+	        user.setName(updatedUser.getName());
+	        user.setEmail(updatedUser.getEmail());
+	        user.setMobile(updatedUser.getMobile());
+	        return userRepository.save(user);
+	    }).orElseThrow(() -> new RuntimeException("User not found with id " + id));
+	}
+	
+	@DeleteMapping("/{id}")		//Endpoint will have the id of the user
+	public String deleteUser(@PathVariable Long id) {
+	    if (userRepository.existsById(id)) {
+	        userRepository.deleteById(id);
+	        return "User with id " + id + " deleted.";
+	    } else {
+	        return "User not found with id " + id;
+	    }
+	}
+
 }
