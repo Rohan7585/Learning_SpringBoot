ddc24e8a4adf0a7ff0f8a00a2bf581c4d729633b
Rohan7585 <rohan178.rd@gmail.com>
2025-05-15 00:30:41 +0530

    Working on Service files

---
+++ ProjectBasedAssigment/PatientAppointmentManagementSystem/src/main/java/com/XYZHospital/PatientAppointmentManagementSystem/Service/PatientService.java
@@ -0,0 +1,12 @@
+package com.XYZHospital.PatientAppointmentManagementSystem.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.XYZHospital.PatientAppointmentManagementSystem.Entity.PatientEntity;
+
+@Service
+public class PatientService {
+
+	
+	
+}
