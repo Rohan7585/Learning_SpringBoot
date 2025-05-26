d30f22fa798640b7b9be9621d4c486b382d8e057
Rohan7585 <rohan178.rd@gmail.com>
2025-05-26 21:03:29 +0530

    Demo Project added for Automatic DTO mapper

---
+++ AutomaticDtoMapper/src/main/java/com/example/AutomaticDtoMapper/AutomaticDtoMapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutomaticDtoMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticDtoMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticDtoMapperApplication.class, args);
+	}
+
+}
---
+++ AutomaticDtoMapper/src/test/java/com/example/AutomaticDtoMapper/AutomaticDtoMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AutomaticDtoMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticDtoMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
