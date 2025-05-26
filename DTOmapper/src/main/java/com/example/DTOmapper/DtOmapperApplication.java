d5a026d9e03d312e6cb10217220e0181aef4eca2
Rohan7585 <rohan178.rd@gmail.com>
2025-05-26 20:23:09 +0530

    Demo Project added for Entity-DTO manual mapping

---
+++ DTOmapper/src/main/java/com/example/DTOmapper/DtOmapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.DTOmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtOmapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtOmapperApplication.class, args);
+	}
+
+}
---
+++ DTOmapper/src/test/java/com/example/DTOmapper/DtOmapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DTOmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtOmapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
